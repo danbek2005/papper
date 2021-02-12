2c274dd561e1a51bd30160775dfddc29e4b835b6
Danyl <danbek20508@gmail.com>
2021-02-12 19:07:03 +0200

    text

--- main.py
+++ main.py
@@ -4,23 +4,31 @@ from tkinter import messagebox
 import os
 
 root = Tk()
-root.geometry('360x360')
+root.geometry('400x400')
 root.title('Aloha')
 
 def yes():
-    gay_text = messagebox.showerror("YES", "Yes you gay☺♥♥")
+    i = 0;
+    while(i < 100):
+        print(i)
+        i += 1
+        gay_text = messagebox.showerror("YES", "YOU ARE GAY BOY")
 def no():
-    gay_text_no = messagebox.showerror("!!!WHY?!!!", "You gay☺☻☺")
+    i = 0;
+    while (i < 100):
+        print(i)
+        i += 1
+        gay_text = messagebox.showerror("YES", "YOU ARE GAY BOY")
 def exit():
     root.destroy()
 
-btn_yes = Button(root, text="YES", bg="red", fg="green", command=lambda: yes())
-btn_yes.place(x=145, y=160)
+btn_yes = Button(root, text="YES", bg="pink", fg="green", command=lambda: yes())
+btn_yes.place(width=100, height=50, x=100, y=185)
 
-btn_no = Button(root, text="NO", bg="blue", fg="green", command=lambda: no())
-btn_no.place(x=145, y=185)
+btn_no = Button(root, text="NO", bg="pink", fg="green", command=lambda: no())
+btn_no.place(width=100, height=50, x=200, y=185)
 
-btn_exit = Button(root, text="GOODBYE", bg="purple", fg="gray", command=lambda: exit())
-btn_exit.place(x=200, y=200)
+btn_exit = Button(root, text="EXIT", bg="pink", fg="gray", command=lambda: exit())
+btn_exit.place(x=350, y=0, width=50, height=25)
 
 root.mainloop()
