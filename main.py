e2c134d8d665a0c5faec92f12cb18f58f71e6410
Quattro465 <lloydfols5@gmail.com>
2021-02-12 16:12:12 +0200

    Hello11

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from tkinter import *
+from tkinter import Button
 import os
 
 root = Tk()
 root.geometry('360x360')
 root.title('Aloha')
+
+def gay():
+    text = Label(root, text="You really gay")
+    text.pack()
+
+btn = Button(root, text="GAY", bg="red", fg="green", command=lambda: gay())
+btn.place(x=145, y=160)
 
 root.mainloop()
