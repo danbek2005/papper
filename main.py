9b2b04bc4360beeba9672ea2c93f1e1d6b576a62
Danyl <danbek20508@gmail.com>
2021-02-13 14:39:01 +0200

    Update

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
+import eel
+import re
+import mechanize
+import urllib3
+from nerodia.browser import Browser
 
-if __name__ == "__main__":
-    print("Hello World")
+
+browser = Browser(browser='chrome')
+browser.goto('https://ftl.kherson.ua/')
+
+browser.link(value='#logout').click()
+username_input = browser.text_field(title='username')
+username_input.value = ''
+password_input = browser.text_field(title='password')
+password_input.value = ''
+browser.button(value="lp-button").click()
+browser.close()
+
+http = urllib3.PoolManager()
+r = http.request('GET', 'https://ftl.kherson.ua/')
+if(r.status == 200):
+    print(r.headers)
+
+
+
+# br = mechanize.Browser()
+# br.open("https://ftl.kherson.ua/")
+# br.click_link(link="https://ftl.kherson.ua/#logout")
+# br.select_form(name="lp-form")
+# br.form['username'] = ""
+# br.form['password'] = ""
+# br.submit()
+
+eel.init("web")
+
+# todo: position and size doesn't work here
+eel.start("main.html", geometry={'size': (200, 200), 'position': (0, 0)})
