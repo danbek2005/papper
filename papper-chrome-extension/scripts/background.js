e7f5f55b7f241ffab31d56ea3add3fb7e3dc9491
Danyl <danbek20508@gmail.com>
2021-03-09 18:23:55 +0200

    commit

--- papper-chrome-extension/scripts/background.js
+++ papper-chrome-extension/scripts/background.js
@@ -1,19 +1,20 @@
-APP_STATUS = true
+APP_STATUS = false
+AUTH_STATUS = false
 
 
 get_domain = (url) => {
-        domain = (new URL(url))
-        domain = domain.hostname
-        return domain
+    domain = (new URL(url))
+    domain = domain.hostname
+    return domain
 }
 
 put_script = (name) => {
-    chrome.tabs.executeScript(null, {file: "scripts/"+name+".js"})
+    chrome.tabs.executeScript(null, { file: "scripts/" + name + ".js" })
 }
 
 track_rebooting = (id, d) => {
-    chrome.tabs.query({status: "loading", tabId: id}, () => {
-        if(domain.get() == d){
+    chrome.tabs.query({ status: "loading", tabId: id }, () => {
+        if (domain.get() == d) {
             put_script("foreground")
         }
     })
@@ -21,64 +22,65 @@ track_rebooting = (id, d) => {
 
 interval = () => {
     console.log(APP_STATUS)
-    chrome.tabs.query({active: true}, tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, {message: "PING"}, res => {
+    chrome.tabs.query({ active: true }, tabs => {
+        chrome.tabs.sendMessage(tabs[0].id, { message: "PING" }, res => {
             console.log(res)
-            if(!res){
+            if (!res) {
                 chrome.tabs.get(tabs[0].id, info => {
-                    if(/^https:\/\/ftl\.kherson\.ua\//.test(info.url)){
+                    if (/^https:\/\/ftl\.kherson\.ua\//.test(info.url)) {
                         put_script("foreground")
                         clearInterval(interval)
                         chrome.extension.onConnect.addListener((port) => {
-                            port.postMessage({type: "status"})
+                            port.postMessage({ type: "status" })
                             port.onMessage.addListener(message => {
                                 console.log(message)
-                                if(message == "200"){
+                                if (message == "200") {
                                     setInterval(interval)
                                 }
                             })
                         })
                     }
-            })
+                })
             }
         })
     })
 }
 
-(function(){
-    chrome.extension.onConnect.addListener((port) => {
-        if(port.name == "POPUP STREAM"){
-            port.onMessage.addListener(message => {
-                console.log(message)
-                if(message.type == "switch_app_status"){
-                    APP_STATUS = message.status
-                }
-            })
-        }
-    })
-    setInterval(interval, 1000)
-})()
+chrome.extension.onConnect.addListener((port) => {
+    if (port.name == "POPUP STREAM") {
+        port.onMessage.addListener(message => {
+            console.log(message)
+            if (message.type == "GET-AUTH-STATUS") {
+                port.postMessage({type: "RETURN-AUTH-STATUS", data: AUTH_STATUS})
+            }
+            if(message.type == "GET-APP-STATUS"){
+                port.postMessage({type: "RETURN-APP-STATUS", data: APP_STATUS})
+            }
+        })
+    }
+})
+    //setInterval(interval, 1000)
 
 //----------------------------------------------------------------------------------
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if(request.message == "send_user_data"){
+    if (request.message == "send_user_data") {
         chrome.windows.create({
-            'url': 'ui_content/save_data_alert.html', 
-            'type': 'popup', 
+            'url': 'ui_content/save_data_alert.html',
+            'type': 'popup',
             'width': 360,
             'height': 360,
             'top': 100,
             'left': (4 * 360)
         }, (window) => {
             chrome.extension.onConnect.addListener((port) => {
-                port.postMessage({type: "save_data", data: request.data})
+                port.postMessage({ type: "save_data", data: request.data })
                 port.onMessage.addListener(message => {
-                    if(message == "close_window"){
-                        chrome.windows.remove(window.id, () => {})
+                    if (message == "close_window") {
+                        chrome.windows.remove(window.id, () => { })
                     }
-                    else if(message == "save_data"){
-                        chrome.storage.sync.set(message.data, () => {})
-                        chrome.windows.remove(window.id, () => {})
+                    else if (message == "save_data") {
+                        chrome.storage.sync.set(message.data, () => { })
+                        chrome.windows.remove(window.id, () => { })
                     }
                 })
             })
--- papper-chrome-extension/scripts/popup.js
+++ papper-chrome-extension/scripts/popup.js
@@ -1,18 +1,26 @@
+function sleep(miliseconds) {
+    var currentTime = new Date().getTime();
+ 
+    while (currentTime + miliseconds >= new Date().getTime()) {
+    }
+ }
+
+
 
 class PopupUI {
 
     life = this
     STREAM = {
         start: () => {
-            life.port = chrome.extension.connect({
+            this.life.port = chrome.extension.connect({
                 name: "POPUP STREAM"
             });
         },
         send: (message) => {
-            life.port.postMessage(message)
+            this.life.port.postMessage(message)
         },
         get: (conditionType, callback) => {
-            life.port.onMessage(message => {
+            this.life.port.onMessage.addListener(message => {
                 if (message.type == conditionType) {
                     callback(message.data)
                 }
@@ -20,118 +28,52 @@ class PopupUI {
         }
     }
     UI = {
-        switch_el = {
+        switch_el: {
             get: () => {
                 return document.getElementById("switch_status").checked
             },
             set: (bool) => {
+                console.log(bool)
                 document.getElementById("switch_status").checked = bool
             }
         },
-        show_auth_status_el = () => {
-                el = document.getElementById("status")
-                if (!life.IS_AUTH) {
-                    el.innerHTML = "<img src='../images/error_el.png' class='status_img'><h4>Not authorized</h4>"
+        show_auth_status_el: () => {
+                let el = document.querySelector("#auth_status")
+                if (!this.life.IS_AUTH) {
+                    el.innerHTML = "<h4 class='auth_msg'>#Not authorized</h4><img class='auth_img' src='images/red.svg'/>"
                 } else {
-                    el.innerHTML = "<img src='../images/200.png' class='status_img'><h4>Authorization is successful</h4>"
+                    el.innerHTML = "<h4 class='auth_msg'>#Authorization is successful</h4><img class='auth_img' src='images/green.svg'/>"
                 }
             }
 
     }
     appStatus = {
         get: () => {
-            life.STREAM.send({ type: "GET-APP-STATUS" })
-            return life.STREAM.get("RETURN-APP-STATUS", (data) => {
-                return data;
+            this.life.STREAM.send({ type: "GET-APP-STATUS" })
+            this.life.STREAM.get("RETURN-APP-STATUS", (data) => {
+                this.life.UI.switch_el.set(data)
             })
         },
         set: (data) => {
-            life.STREAM.send({ type: "SET-APP-STATUS", data: data })
+            this.life.STREAM.send({ type: "SET-APP-STATUS", data: data })
         }
     }
     authStatus = {
         get: () => {
-            life.STREAM.send({ type: "GET-AUTH-STATUS" })
-            return life.STREAM.get("RETURN-AUTH-STATUS", (data) => {
+            this.life.STREAM.send({ type: "GET-AUTH-STATUS" })
+            return this.life.STREAM.get("RETURN-AUTH-STATUS", (data) => {
                 return data;
             })
         }
     }
     constructor() {
-        this.startStream()
-        this.UI.switch_el = this.appStatus
+        this.STREAM.start()
+        this.appStatus.get()
         this.IS_AUTH = this.authStatus
         this.UI.show_auth_status_el()
     }
 }
 
-
-
-app_status = localStorage.getItem("APP_STATUS")
-main_auth_status = false
-data = {}
-
-switch_auth_form = (bool) => {
-    if (bool) {
-        auth.style.display = "block";
-    } else {
-        auth.style.display = "none";
-    }
-}
-
-switch_status = (bool) => {
-    el = document.getElementById("status")
-    if (bool) {
-        el.style.display = "block"
-    } else {
-        el.style.display = "none"
-    }
-}
-
-set_ui_status = () => {
-    el = document.getElementById("status")
-    if (!main_auth_status) {
-        el.innerHTML = "<img src='../images/error_el.png' class='status_img'><h4>Not authorized</h4>"
-    } else {
-        el.innerHTML = "<img src='../images/200.png' class='status_img'><h4>Authorization is successful</h4>"
-    }
-}
-
-setInterval(() => {
-    app_status = localStorage.setItem("APP_STATUS")
-}, 1000)
-
-openstream = () => {
-    port = 
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("auth_btn").addEventListener("click", () => {
-        data.mail = mail.value
-        data.password = password.value
-        alert(data.mail)
-    })
-
-    set_ui_status()
-    if (!main_auth_status) {
-        switch_auth_form(true)
-    }
-    port = chrome.extension.connect({
-        name: "POPUP STREAM"
-    });
-    port.postMessage("YOYO")
-    // switch_el = document.getElementById("switch_status")
-    // switch_parent = document.getElementsByClassName("switch")[0]
-
-    // if(localStorage.getItem("APP_STATUS") == false){
-    //     switch_parent.innerHTML = "<input type='checkbox' id='switch_status'>\
-    //     <span class='slider round'></span>"
-    // }
-
-    // switch_el.addEventListener("click", (el) => {
-    //     console.log(app_status)
-    //     app_status = !app_status
-    //     localStorage.setItem("APP_STATUS", app_status);
-    //     port.postMessage({type: "switch_app_status", status: app_status})
-    // })
+    popup = new PopupUI()
 })
